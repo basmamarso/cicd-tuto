bf47a04133d1167ea537581411f311f6f58b8da8
basmamarso <118186582+basmamarso@users.noreply.github.com>
2022-11-23 14:46:50 +0100

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -13,10 +13,7 @@ print("3.Multiply")
 print("4.Divide")
 
 while True:
- 
     choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
